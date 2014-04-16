301404889e2ca20e5e08785c33a4f69011116fef
rphlypo <Ronald.Phlypo@inria.fr>
2014-04-16 19:06:17 +0200

    more htree

--- htree.py
+++ htree.py
@@ -1,41 +1,51 @@
 import collections
-from sklearn.base import BaseEstimator, TransformerMixin
 
 
-class Node(BaseEstimator, TransformerMixin):
-    def __init__(self, parent=None, children=[]):
+class Node(object):
+    def __init__(self, parent=None, children=[], value=None):
         self.parent = parent
         self.children = children
+        self.value = value
 
-    def fit(self, X):
-        self.value_ = evaluate(self)
-        return self
+    def evaluate(self):
+        return set([child.evaluate()
+                    if child.children
+                    else child.value
+                    for child in self.children])
+
+    def add_children(self, children):
+        """ add one or more children
+        """
+        print "adding children {} to {}".format(children, self)
+        if isinstance(children, collections.Iterable):
+            self.children.extend(children)
+        else:
+            self.children.append(children)
+
+    def complement(self):
+        return self.parent.evaluate - self.evaluate
+
+    def set_value(self, value):
+        self.value = value
 
 
-def evaluate(node, current_values=[]):
-    current_values.extend([evaluate(child)
-                           for child in node.children])
-    return set(current_values)
+class HTree(object):
+    def __init__(self, root=None, tree_list=None):
+        self.root = root
+        self.tree_list = tree_list
+        self.expand()
 
-
-def complement(node):
-    return node.parent.fit().value_ - node.fit().value_
-
-
-class HTree(BaseEstimator, TransformerMixin):
-    def __init__(self, root=None):
-        return None
-
-    def fit(self, X, y=None):
+    def expand(self):
         """ get the root node right, develop the tree using transform()
         """
-        if (not isinstance(X, collections.Iterable) or len(X) == 1):
-            self.root_ = Node(parent=self.root)
+        if (not isinstance(self.tree_list, collections.Iterable)
+                or len(self.tree_list) == 1):
+            self.root_ = Node(parent=self.root, value=self.tree_list,
+                              children=[])
         else:
-            self.root_ = Node(parent=self.root,
-                              children=[HTree(tree) for tree in X])
+            self.root_ = Node(parent=self.root)
 
-    def transform(self, X):
-        return {self.root_} & set([Node(parent=self.root_,
-                                        children=_expand(children.fit()))
-                                   for children in self.root_.children])
+            children = [HTree(root=self.root_, tree_list=tree).root_
+                        for tree in self.tree_list]
+            self.root_.add_children(children)
+        return self
