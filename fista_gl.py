0676af4a8cd100e8348d002b4db14f6a862029c4
rphlypo <Ronald.Phlypo@inria.fr>
2014-07-29 12:10:57 +0200

    added alpha_max

--- fista_gl.py
+++ fista_gl.py
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 import scipy.linalg
 
 
-gl = covl.GraphLasso(alpha=.4, base_estimator=LedoitWolf(assume_centered=True),
-                     mu=2., tol=1e-3, rho=1.)
-
-
-def CVpath(subj_dir, cov_learner=gl, session=1, n_alphas=51, n_iter=3,
+def CVpath(subj_dir, cov_learner=None, session=1, n_alphas=51, n_iter=3,
            use_init=True):
+    if cov_learner is None:
+        cov_learner = covl.GraphLasso(
+            alpha=.1,
+            base_estimator=EmpiricalCovariance(assume_centered=True))
     X = ipen._get_train_ix(subj_dir, n_iter=n_iter)
     ipen.alpha_max(X)
     score_path = np.zeros((n_alphas,))
@@ -65,18 +65,21 @@ def fit_model(X_train, X_test, alpha, cov_learner, use_init=None):
         inits = (Z, U)
     else:
         inits = None
-    msg += '\ngraphical lasso has set rho to {}'.format(cov_learner.rho)
+    msg += '\ngraphical lasso has set rho to {}'.format(cov_learner.rho_[-1])
     score = cov_learner.score(X_test)
-    msg += '\nscore = {}'.format(score)
+    msg += '\nscore = {} ({} iterations)'.format(score,
+                                                 len(cov_learner.f_vals_))
     print msg
     return score, inits
 
 
-def fista_graph_lasso(X, alpha, L=.1, eta=1.1, Theta_init=None,
-                      precision=1e-6):
+def fista_graph_lasso(X, alpha, L=.01, eta=1.1, Theta_init=None,
+                      precision=1e-2, Hessian=False, max_iter=1e2,
+                      base_estimator=EmpiricalCovariance(
+                          assume_centered=True), fista=False):
+    # incorporate the Hessian ...
     t = 1.
-    empcov = EmpiricalCovariance(assume_centered=True)
-    covX = empcov.fit(X).covariance_
+    covX = base_estimator.fit(X).covariance_
     p = covX.shape[0]
     penalty_mask = np.ones((p, p)) - np.identity(p)
     alpha = alpha * penalty_mask
@@ -85,29 +88,57 @@ def fista_graph_lasso(X, alpha, L=.1, eta=1.1, Theta_init=None,
     else:
         Theta = Theta_init
     S = covl._cov_2_corr(covX)
+    print 'alpha_max = {}'.format(alpha_max(S))
+    alpha *= alpha_max(S)
     G = grad_f_graphlasso(Theta, S)
     Z = pL_graphlasso(Theta, alpha, G, L)
 
+#    if Hessian:
+#        sqrtS = sqrtm(S)
+#        max_eval = np.max(scipy.linalg.schur(
+#            sqrtS.dot(Theta.dot(sqrtS)))[0])
+#        L = max(L, max_eval)
+
+    f_vals_ = [f_graphlasso(Theta, S) + g_graphlasso(Theta, alpha)]
+    print 'F = {} (t = {})'.format(f_vals_[-1], t)
+
+    n_iter = 0
     while True:
         try:
-            Theta_ = scipy.linalg.eigh(Theta)
+            n_iter += 1
+            if not Hessian:
+                Theta_ = scipy.linalg.eigh(Theta)
+            else:
+                Theta_ = Theta
             Z_old = Z.copy()
-            G = grad_f_graphlasso(Theta_, S)
+            G = grad_f_graphlasso(Theta_, S, Hessian=Hessian)
             Z = pL_graphlasso(Theta, alpha, G, L)
             fTheta = f_graphlasso(Theta_, S)
             fZ = f_graphlasso(Z, S)
-            while fZ > Q_graphlasso(Z, Theta_, G, L, alpha, fTheta,
+            while fZ > Q_graphlasso(Z, Theta, G, L, alpha, fTheta,
                                     penalty_mask):
                 L *= eta
+#                if Hessian:
+#                    max_eval = np.max(scipy.linalg.schur(
+#                        sqrtS.dot(Theta.dot(sqrtS)))[0])
+#                    L = max(L, max_eval)
                 Z = pL_graphlasso(Theta, alpha, G, L)
                 fZ = f_graphlasso(Z, S)
+            if fista:
                 t_old = t
                 t = 0.5 + np.sqrt(1 + 4 * t ** 2) / 2
-            Theta = Z + (t_old - 1) / t * (Z - Z_old)
-            if t < precision:
+                Theta = Z + (t_old - 1.) / t * (Z - Z_old)
+            else:
+                Theta = Z
+            f_vals_.append(f_graphlasso(Theta, S) +
+                           g_graphlasso(Theta, alpha))
+            print 'F = {} (t = {}, L = {})'.format(f_vals_[-1], t, L)
+            if 1. / t < precision or n_iter >= max_iter:
+                raise StopIteration
+            if len(f_vals_) > 1 and f_vals_[-2] - f_vals_[-1] < precision:
                 raise StopIteration
         except StopIteration:
-            return Theta
+            return Theta, f_vals_
 
 
 def f_graphlasso(Theta, S):
@@ -123,11 +154,18 @@ def g_graphlasso(Theta, alpha):
     return np.sum(np.abs(Theta * alpha))
 
 
-def grad_f_graphlasso(Theta, S):
+def grad_f_graphlasso(Theta, S, Hessian=False):
     if not isinstance(Theta, np.ndarray):
-        return S - Theta[1].dot(np.diag(1. / Theta[0])).dot(Theta[1].T)
+        if not Hessian:
+            return S - Theta[1].dot(np.diag(1. / Theta[0])).dot(Theta[1].T)
+        else:
+            Theta = Theta[1].dot(np.diag(Theta[0])).dot(Theta[1])
+            return grad_f_graphlasso(Theta, S, Hessian=Hessian)
     else:
-        return S - scipy.linalg.inv(Theta)
+        if not Hessian:
+            return S - scipy.linalg.inv(Theta)
+        else:
+            return Theta.dot(S.dot(Theta)) - Theta
 
 
 def pL_graphlasso(Theta, alpha, grad, L):
@@ -145,9 +183,51 @@ def Q_graphlasso(Z, Theta, grad, L, alpha, f_Theta, pen_mask=None):
     return Q
 
 
+def sqrtm(X):
+    evals, evecs = scipy.linalg.eigh(X)
+    return evecs.dot(np.diag(np.sqrt(evals)).dot(evecs.T))
+
+
+def _check_mx(X, props='pos_sym_2d_sq'):
+    msg = ''
+    if 'pos' in props and np.min(np.diag(scipy.linalg.schur(X)[0])) < 0.:
+        msg += 'matrix is not positive definite'
+    if 'sym' in props:
+        try:
+            np.assert_array_almost_equal(X, X.T)
+        except AssertionError:
+            if msg:
+                msg += ', '
+            msg += 'matrix is not symmetric'
+    if '2d' in props and X.ndim != 2:
+        if msg:
+            msg += ', '
+        msg += 'matrix has to many dimensions'
+    if 'sq' in props and not np.allclose(X.shape - X.shape[0],
+                                         np.zeros((X.ndim,))):
+        if msg:
+            msg += ', '
+        msg += 'matrix is not square'
+    if msg:
+        raise ValueError(msg)
+
+
+def alpha_max(X):
+    p = X.shape[0]
+    X[::p + 1] = 0.
+    return np.max(np.abs(X[:]))
+
+
 if __name__ == '__main__':
     import cProfile
     import pstats
+
+    gl = covl.GraphLasso(
+        alpha=.4,
+        # base_estimator=LedoitWolf(assume_centered=True),
+        base_estimator=EmpiricalCovariance(assume_centered=True),
+        mu=2., tol=1e-4, rho=1.)
+
     pr1 = cProfile.Profile()
     subj_dir = chp.subject_dirs[0]
     pr1.enable()
