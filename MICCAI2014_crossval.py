17036ec7f874df68a775f9bb22117931d337864f
Ronald Phlypo <ronald.phlypo@inria.fr>
2014-12-01 09:28:20 +0100

    running analysis using previously obtained preprocessing results

--- MICCAI2014_crossval.py
+++ MICCAI2014_crossval.py
@@ -1,10 +1,12 @@
-import numpy as np
-import covariance_learn as cvl
-import scipy.linalg as linalg
+# import numpy as np
+# import covariance_learn as cvl
+# import scipy.linalg as linalg
 from joblib import Parallel, Memory, delayed
 import glob
-from sklearn.utils import check_random_state
+# from sklearn.utils import check_random_state
 import os
+
+import compute_hierarchical_parcellation as chp
 
 from compute_hierarchical_parcellation import get_data
 
@@ -23,14 +25,17 @@ def cross_val(subject_dir, labels_img="labels_level_3.nii.gz",
               method=None, sess_ix=None,
               random_state=None, **kwargs):
     print "processing {} with atlas {}".format(subject_dir, labels_img)
-    randgen = check_random_state(random_state)
+    # randgen = check_random_state(random_state)
     get_data_ = mem.cache(get_data)
-    subj_data = get_data_(subject_dir, labels_img=labels_img)
-    return subj_data
+    results = chp.run_analysis(subject_dirs=subject_dirs, get_data_=get_data_)
+    # subj_data = get_data_(subject_dir, labels_img=labels_img)
+    return results
 
 
 if __name__ == "__main__":
-    Parallel(n_jobs=20)(delayed(cross_val)(sd)
-                        for sd in subject_dirs)
-    Parallel(n_jobs=20)(delayed(cross_val)(sd, labels_img='HOmaps.nii.gz')
-                        for sd in subject_dirs)
+    res = {'hier': [], 'HO': []}
+    res['hier'] = Parallel(n_jobs=20)(delayed(cross_val)(sd)
+                                      for sd in subject_dirs)
+    res['HO'] = Parallel(n_jobs=20)(delayed(cross_val)(
+                                    sd, labels_img='HOmaps.nii.gz')
+                                    for sd in subject_dirs)
